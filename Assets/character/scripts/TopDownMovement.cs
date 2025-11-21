ac8496b8e30e788b85a9609a31dce0d59f1c4289
a25milla <a25milla@student.his.se>
2025-11-21 15:39:34 +0100

    SFX

--- Assets/character/scripts/TopDownMovement.cs
+++ Assets/character/scripts/TopDownMovement.cs
@@ -5,25 +5,27 @@ using UnityEngine.InputSystem;
 // This script automatically adds a Rigidbody2D and a CapsuleCollider2D component in the inspector.
 // The following components are needed: Player Input
 
-[RequireComponent(typeof(Rigidbody2D), typeof(CapsuleCollider2D))]
-public class TopDownMovement : MonoBehaviour
-{
-    public float maxSpeed = 7;
-    
-    public bool controlEnabled { get; set; } = true;    // You can edit this variable from Unity Events
-    
-    private Vector2 moveInput;
-    private Rigidbody2D rb;
-    
-    void Awake()
-    {
-        rb = GetComponent<Rigidbody2D>();
-        
-        // Set gravity scale to 0 so player won't "fall" 
-        rb.gravityScale = 0;
-    }
+            [RequireComponent(typeof(Rigidbody2D), typeof(CapsuleCollider2D))]
+            public class TopDownMovement : MonoBehaviour
+            {
+                public float maxSpeed = 7;
 
-    private void Update()
+                public bool controlEnabled { get; set; } = true; // You can edit this variable from Unity Events
+
+                private Vector2 moveInput;
+                private Rigidbody2D rb;
+
+                void Awake()
+                {
+                    rb = GetComponent<Rigidbody2D>();
+
+                    // Set gravity scale to 0 so player won't "fall" 
+                    rb.gravityScale = 0;
+
+              
+            }
+
+            private void Update()
     {
         
     }
@@ -44,6 +46,7 @@ public class TopDownMovement : MonoBehaviour
         // Write code for walking animation here. (Suggestion: send your current velocity into the Animator for both the x- and y-axis.)
     }
     
+
     // Handle Move-input
     // This method can be triggered through the UnityEvent in PlayerInput
     public void OnMove(InputAction.CallbackContext context)
